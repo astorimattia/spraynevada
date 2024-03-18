d471817a2f9bc92576bff83c3124ef7c440a7391
astorimattia <mattiastori@gmail.com>
2024-03-17 22:19:15 -0700

    moved index out

--- index.tsx
+++ index.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hero from './components/Hero';
-import Drone from './components/Drone';
-import CaseStudies from './components/CaseStudies';
+import Hero from './src/components/Hero';
+import Drone from './src/components/Drone';
+import CaseStudies from './src/components/CaseStudies';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

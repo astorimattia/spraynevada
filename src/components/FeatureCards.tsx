cdc93a137ffff55c8616d6f7ddf8f172ded132e7
astorimattia <mattiastori@gmail.com>
2024-03-24 11:23:46 -0700

    cards gap and copy

--- src/components/FeatureCards.tsx
+++ src/components/FeatureCards.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const FeatureCards = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center gap-6 p-4">
+    <div className="flex flex-col md:flex-row justify-center items-center gap-4 md:gap-6 p-4">
       <div className="flex flex-col items-center gap-4 p-6 rounded-xl w-64 h-48">
-        <div className="relative text-white text-6xl font-medium">50%</div>
-        <div className="text-center text-white text-opacity-60 text-lg font-medium">Less chemical applied</div>
+        <div className="relative text-white text-6xl font-bolf">50%</div>
+        <div className="text-center text-white text-opacity-60 text-lg font-medium">Less chemical applied on crops</div>
       </div>
       <div className="flex flex-col items-center gap-4 p-6 rounded-xl w-64 h-48">
         <div className="relative">

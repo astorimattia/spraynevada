174444f91d310c1dff60b116c538e2967077650a
astorimattia <mattiastori@gmail.com>
2024-03-24 09:59:00 -0700

    case study fix

--- src/data/caseStudies.jsx
+++ src/data/caseStudies.jsx
@@ -1,21 +1,21 @@
 // data/caseStudies.jsx
 export const caseStudies = [
   {
-    title: 'Wheat Weed Control',
-    description: 'Drone was used for effective weed control in wheat fields, covering 53.3 hectares in Shandong, China. The operation involved precise spraying of herbicides, resulting in successful weed eradication without harming the wheat.',
+    title: 'Precision Weed Management Wheat Fields',
+    description: 'Discover how drones can effectively control weeds wheat fields, covering large areas with precise herbicide application. Our case study demonstrates successful weed eradication without damaging the crops.',
     image: 'case1.webp',
-    link: '/case-studies/wheat-weed-control',
+    link: 'https://ag.dji.com/case-studies/dji-ag-case-en-wheat',
   },
   {
-    title: 'Weed Control for Winter Wheat',
-    description: 'This article provides tips on UAV plant protection for winter wheat, including weed classification, herbicide selection, and precautions for spraying. It emphasizes the importance of timing, weather conditions, and herbicide choice for effective weed control.',
+    title: 'Optimizing Winter Wheat Protection',
+    description: "Learn best practices to protect winter wheat. This article covers weed classification, herbicide selection, and essential considerations for effective aerial spraying in the region's unique climate and conditions.",
     image: 'case2.webp',
-    link: '/case-studies/weed-control-winter-wheat',
+    link: 'https://ag.dji.com/case-studies/dji-ag-case-en-wheat-fields',
   },
   {
-    title: 'Controlling Corn Borers',
-    description: 'An outbreak of corn borers in Bole City, Xinjiang, China was controlled using DJI\'s MG-1S drone for aerial spraying over 2,735 acres, resulting in an 80% mortality rate of the pests.',
+    title: 'Combating Corn Pests with Drone Technolog',
+    description: 'See how drones can be deployed to control devastating corn borer outbreaks. Our case study showcases the effectiveness of aerial spraying in reducing pest populations and protecting valuable corn crops.',
     image: 'case3.webp',
-    link: '/case-studies/controlling-corn-borers',
+    link: 'https://ag.dji.com/case-studies/dji-ag-case-en-corn',
   },
 ];  

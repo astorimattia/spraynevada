7823eb1f0e2aa080e65cb6920fa435837e46f151
astorimattia <mattiastori@gmail.com>
2024-03-24 12:26:21 -0700

    sparkles and framer motion and logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,9 +13,16 @@ const Header = () => {
   };
 
   return (
-    <header className="top-0 left-0 right-0 z-50 opacity-80">
+    <header className="top-0 left-0 right-0 z-50 opacity-90">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-center items-center py-4">
+        <div className="flex justify-between items-center py-4">
+          <div className="flex items-center">
+            <img
+              src="white_logo.png"
+              alt="Logo"
+              className="h-8 w-8 md:h-12 md:w-12 mr-4"
+            />
+          </div>
           <div className="hidden md:flex space-x-8">
             <button
               className="text-white hover:text-purple-500 font-bold"
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,6 @@
 // components/Hero.tsx
 import React from 'react';
+import { SparklesCore } from './sparkles';
 
 const Hero = () => {
   const handleLearnMoreClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
@@ -12,9 +13,20 @@ const Hero = () => {
 
   return (
     <div id="hero-section" className="-mt-12 flex items-center justify-center min-h-screen relative flex-grow">
-    <div style={{ position: 'absolute', top: -10, left: 0, width: '100%', height: '100%', background: 'radial-gradient(rgba(141, 39, 221, 0.49), rgba(25, 20, 32, 0.0) 68%)', zIndex: 0, }} />
+      <div style={{ position: 'absolute', top: -10, left: 0, width: '100%', height: '100%', background: 'radial-gradient(rgba(39, 221, 48, 0.2), rgba(20, 32, 23, 0.0) 68%)', zIndex: 0, }} />
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.2}
+          particleDensity={80}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
     <div className="text-center z-10">
-        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-[#CAA3FA]">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-[#a3faa7]">
           Spray Smarter <br /> With Drone Application
         </h1>
         <div className="text-white font-medium text-xl md:text-2xl opacity-60 mt-6 md:mt-8">
